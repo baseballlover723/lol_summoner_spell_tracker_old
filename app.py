688fda1216eb5581531260ce02bf26fa7180fe67
Philip Ross <dev@baseballlover723.com>
2018-05-05 00:06:39 -0700

    added logging

--- app.py
+++ app.py
@@ -1,12 +1,61 @@
 from dotenv import load_dotenv
 load_dotenv(verbose=True, dotenv_path="./.env")
-from flask import Flask, render_template
+from flask import Flask, render_template, g, request
 from server.helper import *
 from pprint import pprint
 import cassiopeia as cass
 import os
 
+import datetime
+import time
+
+import colors
+from rfc3339 import rfc3339
+
 app = Flask(__name__, static_folder="./dist")
+app.debug = True
+
+@app.before_request
+def start_timer():
+    g.start = time.time()
+
+
+@app.after_request
+def log_request(response):
+    if request.path == '/favicon.ico':
+        return response
+    elif request.path.startswith('/static'):
+        return response
+
+    now = time.time()
+    duration = round(now - g.start, 2)
+    dt = datetime.datetime.fromtimestamp(now)
+    timestamp = rfc3339(dt, utc=True)
+
+    args = dict(request.args)
+
+    log_params = [
+        ('method', request.method, 'yellow'),
+        ('path', request.path, 'yellow'),
+        ('status', response.status_code, 'cyan'),
+        ('duration', duration, 'green'),
+        ('time', timestamp, 'magenta'),
+        ('params', args, 'yellow')
+    ]
+
+    request_id = request.headers.get('X-Request-ID')
+    if request_id:
+        log_params.append(('request_id', request_id, 'yellow'))
+
+    parts = []
+    for name, value, color in log_params:
+        part = colors.color("{}={}".format(name, value), fg=color)
+        parts.append(part)
+    line = " ".join(parts)
+
+    app.logger.info(line)
+
+    return response
 
 @app.route("/")
 def index():
@@ -34,4 +83,4 @@ def in_game(region, summoner_name):
     return region + ", " + summoner_name
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
